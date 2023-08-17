fe7221afecbef93c94984caef6de0fb46ba8fe3b
Nico <franckennicolas@gmail.com>
2023-08-17 18:45:28 -0300

    update 3

--- createDatabase.js
+++ createDatabase.js
@@ -4,44 +4,30 @@ const fs = require("fs");
 const { renderUrl } = require("./config");
 
 async function createDatabase() {
-  // const pool = new Pool({
-  //   user: db.user,
-  //   password: db.password,
-  //   host: db.host,
-  //   port: db.port,
-  //   database: db.postgresDatabase,
-  // });
-
   try {
-    // const result = await pool.query(
-    //   "SELECT datname FROM pg_database WHERE datname = $1",
-    //   ["nicasourcedb"]
-    // );
-
-    // if (result.rows.length === 0) {
-    //   await pool.query(`CREATE DATABASE nicasourcedb`);
-    // } else {
-    //   return;
-    // }
-
     const newPool = new Pool({
-      // user: db.user,
-      // password: db.password,
-      // host: db.host,
-      // port: db.port,
-      // database: db.database,
-
       connectionString: renderUrl,
     });
 
-    const sqlScriptPath = path.join(__dirname, "./database/db.sql");
-    const sqlScript = fs.readFileSync(sqlScriptPath, "utf8");
-    await newPool.query(sqlScript);
-    newPool.end();
+    const result = await newPool.query(`
+    SELECT EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_name = 'creators'
+    );
+  `);
+
+    if (result.rows.length === 0) {
+      newPool.end();
+      return;
+    } else {
+      const sqlScriptPath = path.join(__dirname, "./database/db.sql");
+      const sqlScript = fs.readFileSync(sqlScriptPath, "utf8");
+      await newPool.query(sqlScript);
+      newPool.end();
+    }
   } catch (e) {
     console.log("There was an error", e);
-  } finally {
-    newPool.end();
   }
 }
 
--- db.js
+++ db.js
@@ -2,12 +2,6 @@ const { Pool } = require("pg");
 const { renderUrl } = require("./config");
 
 const pool = new Pool({
-  // user: db.user,
-  // password: db.password,
-  // host: db.host,
-  // port: db.port,
-  // database: db.database,
-
   connectionString: renderUrl,
 });
 
