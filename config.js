264495dd90e0a982da2250147abf1cd40efb3dcc
Nico <franckennicolas@gmail.com>
2023-08-17 19:13:50 -0300

    update 4

--- config.js
+++ config.js
@@ -2,14 +2,6 @@ const { config } = require("dotenv");
 config();
 
 module.exports = {
-  db: {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    postgresDatabase: process.env.DB_POSTGRESDATABASE,
-  },
   port: process.env.PORT,
   secretKey: process.env.SECRET_KEY,
   renderUrl: process.env.RENDER_URL,
