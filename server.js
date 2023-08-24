63bf9fbb6bf875919ebd734f55dea6bbcc6e9913
Nico <franckennicolas@gmail.com>
2023-08-24 15:00:59 -0300

    verify

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const app = express();
 
 app.use(
   cors({
-    // origin: "http://localhost:3000",
-    origin: "https://nicavideo-webapp.netlify.app",
+    origin: "http://localhost:3000",
+    // origin: "https://nicavideo-webapp.netlify.app",
     credentials: true,
   })
 );
